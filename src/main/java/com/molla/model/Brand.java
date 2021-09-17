abf1c80c5f840891af10bfdb3de23b2c9afd3148
mohamed085 <mohamed085555@gmail.com>
2021-09-17 17:04:19 +0200

    Add Brand entity

---
+++ src/main/java/com/molla/model/Brand.java
@@ -0,0 +1,37 @@
+package com.molla.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "brands")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 45, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 128)
+    private String logo;
+
+    @ManyToMany
+    @JoinTable(
+            name = "brands_categories",
+            joinColumns = @JoinColumn(name = "brands_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
+
+
+}
