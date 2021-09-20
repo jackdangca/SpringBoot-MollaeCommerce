983a1b22cce59c9265bec42c4ea4e1908b5ffa5a
mohamed085 <mohamed085555@gmail.com>
2021-09-20 13:38:22 +0200

    Create Product entity

---
+++ src/main/java/com/molla/model/Product.java
@@ -0,0 +1,66 @@
+package com.molla.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 256, nullable = false)
+    private String name;
+
+    @Column(unique = true, length = 256, nullable = false)
+    private String alias;
+
+    @Column(length = 512, nullable = false, name = "short_description")
+    private String shortDescription;
+
+    @Column(length = 4096, nullable = false, name = "full_description")
+    private String fullDescription;
+
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    @Column(name = "updated_time")
+    private Date updatedTime;
+
+    private boolean enabled;
+
+    @Column(name = "in_stock")
+    private boolean inStock;
+
+    private float cost;
+    private float price;
+
+    @Column(name = "discount_percent")
+    private float discountPrice;
+
+    private float length;
+    private float width;
+    private float height;
+    private float weight;
+
+    @Column(name = "main_image", nullable = false)
+    private String mainImage;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+}
