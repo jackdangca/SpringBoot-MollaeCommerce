c672c3188ce47168c3437de6fc2a4fc1ef1aceba
mohamed085 <mohamed085555@gmail.com>
2021-09-20 13:17:29 +0200

    Refactor config

--- src/main/java/com/molla/config/MvcConfig.java
+++ src/main/java/com/molla/config/MvcConfig.java
@@ -12,33 +12,21 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String dirName = "user-photos";
-        Path userPhotosDir = Paths.get(dirName);
-
-        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-
-        registry
-                .addResourceHandler("/" + dirName + "/**")
-                .addResourceLocations("file:/" + userPhotosPath + "/");
-
-        String categoryImagesDirName = "category-images";
-        Path categoryImagesDir = Paths.get(categoryImagesDirName);
-
-        String categoryImagePath = categoryImagesDir.toFile().getAbsolutePath();
-
-        registry
-                .addResourceHandler("/" + categoryImagesDirName + "/**")
-                .addResourceLocations("file:/" + categoryImagePath + "/");
-
-
-        String brandImagesDirName = "brand-logos";
-        Path brandImagesDir = Paths.get(brandImagesDirName);
-
-        String brandImagePath = brandImagesDir.toFile().getAbsolutePath();
-
-        registry
-                .addResourceHandler("/" + brandImagesDirName + "/**")
-                .addResourceLocations("file:/" + brandImagePath + "/");
+        exposeDirectory("user-photos", registry);
+        exposeDirectory("category-images", registry);
+        exposeDirectory("brand-logos", registry);
 
     }
+
+    private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+
+        Path path = Paths.get(pathPattern);
+        String absolutePath = path.toFile().getAbsolutePath();
+
+        String logicalPath = pathPattern.replace("../", "") + "/**";
+
+        registry.addResourceHandler(logicalPath)
+                .addResourceLocations("file:/" + absolutePath + "/");
+    }
+
 }
